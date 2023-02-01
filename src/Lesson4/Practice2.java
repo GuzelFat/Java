f84c2d198c4a32d720be24e59a525cde9735076f
GuzelFat <fatikhova.guzel@mail.ru>
2023-02-01 19:25:08 +0300

    Коррекция файлов ДЗ

--- src/Lesson4/Practice2.java
+++ src/Lesson4/Practice2.java
@@ -1,4 +1,27 @@
 package Lesson4;
 
+// Создать файл с текстом и считать его в терминал
+import java.io.FileReader;
+        import java.io.FileWriter;
+        import java.io.IOException;
 public class Practice2 {
+    public static void main(String[] args) {
+        String str = "";
+        try {
+            FileWriter wr = new FileWriter("NewFile.txt");
+            wr.append("Тестовое задание\n");
+            wr.append("Что-то ещё\n");
+            wr.flush();
+
+
+            FileReader rd = new FileReader("NewFile.txt");
+            while (rd.ready()) {
+                str += (char)rd.read();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(str);
+    }
 }
